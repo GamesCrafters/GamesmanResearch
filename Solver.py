1474e2931355d9f958223e92e37a9ba615dd5c6f
Mpearson765 <42828457+Mpearson765@users.noreply.github.com>
2018-12-07 02:13:20 -0800

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -1,7 +1,14 @@
+# Functions required of all games: primitive(position), generate_moves(position), do_move(position, move)
+# Variables required of all games: START_POS    --This can be removed if someone wants to clean up the functions play() and printViz()
+# Function required for PrintViz: stringify(position)
+# If you plan to use lists in your implementations of your positions or moves DONT. Use Tuples instead. Lists will cause errors.
 
+# Returns all the positions that one can get to from a given position
+# Inputs: The position you want to get the children of.
 def down_child(position):
     return [do_move(position, m) for m in generate_moves(position)]
 
+# Returns all possible positions that can be reached from the certain position of a game, typically the start_pos
 def all_positions(initial_position):
     all_ps, old_all_ps, cur_positions = [initial_position], [], [initial_position]
     while all_ps != old_all_ps:
@@ -14,6 +21,8 @@ def all_positions(initial_position):
                         cur_positions += [child]
     return all_ps
 
+# Solves a position of a game. Assumes no draws in the game from the position and below
+# Inputs: the position you want to solve for.
 def solve(position):
     if primitive(position) != 'UNDECIDED':
         return primitive(position)
@@ -27,6 +36,9 @@ def solve(position):
         else:
             return 'LOSE'
 
+# Solves a game for draw level 0 and returns a dictionary [position : value]. The values are "WIN", "LOSE", "TIE", and "DRAW".
+# values cannot be anything besides one of the four listed
+# Inputs: the initial position of the game.
 def up_solve(position):
     mover = {}
     for p in all_positions(position):
@@ -47,6 +59,10 @@ def up_solve(position):
             mover[key] = 'DRAW'
     return mover
 
+# solves the whole game in entirety and returns a dictionary of [position : value]. Every position is given Win Lose Tie and Draw Level
+# format described for value is the python operation of "DRAW" * N + "WIN OR LOSE OR TIE" where N is the Draw Level of the position.
+# Ex format: "DRAW DRAW LOSE"
+# Inputs: the initial position of the game
 def up_solve_draw_world_one_tier(position):
     mover = {}
     for p in all_positions(position):
@@ -89,15 +105,24 @@ def up_solve_draw_world_one_tier(position):
     return mover
 us = up_solve_draw_world_one_tier
 
+# helper function that makes calculating remoteness in up_remote much easier. Returns -1 so that we can get a remoteness of zero
+# if primitive or the best possible remoteness given the possible values. Best is determined by the function minmax which is just either
+# the min function or the max function
+# Inputs: minmax = either the min function or the max function, lst = a list of remotenesses that are still subject to change.
 def aminax(minmax, lst):
     if lst:
         return minmax(lst)
     else:
         return -1
 
-def up_remote(su, aps):
+# Returns a dictionary of [position : Remoteness] for all positions of draw level 0. All other positions are in the dictionary but have
+# remoteness float('inf')
+# Inputs: su = a dictionary of type [position : value] where the values are of the format that return from up_solve_draw_world_one_tier,
+#              where the value also holds information about the draw level, or of the type that come from up_solve(), where the values 
+#              are limited, but up_solve_draw_world_one_tier just gives more information so it is better and preferred.
+def up_remote(su):
     remoteness_dic, ol_remoteness_dic = {}, {}
-    for p in aps:
+    for p in su.keys():
         if primitive(p) != 'UNDECIDED':
             remoteness_dic[p] = 0
         elif su[p] not in ['WIN', 'LOSE', 'TIE']:
@@ -116,9 +141,14 @@ def up_remote(su, aps):
                 remoteness_dic[p] = aminax(max, [remoteness_dic[_] for _ in children if su[_] == 'WIN']) + 1
     return remoteness_dic
 
-def draw_remote(su, aps):
+# A function that returns a dictionary of [position : remoteness] for all position in draw level 1 where the remoteness of the fringe 
+# of draw level 1 is 0. all other positions are in there and if the other positions have draw level 0 then their value is 0 and if
+# the other positions have draw level >= 2 their value is "UNDECIDED"
+# Inputs: su = a dictionary of type [position : value] where the values are of the format that return from up_solve_draw_world_one_tier,
+#              where the value also holds information about the draw level.
+def draw_remote(su):
     remoteness_dic, ol_remoteness_dic = {}, {}
-    for p in aps:
+    for p in su.keys():
         if su[p] in ['WIN', 'LOSE', 'TIE']:
             remoteness_dic[p] = 0
         elif 'WIN' in child_solver(p, su):
@@ -135,6 +165,12 @@ def draw_remote(su, aps):
                 remoteness_dic[p] = aminax(max, [remoteness_dic[q] for q in children if su[q] == 'DRAW WIN']) + 1
     return remoteness_dic
 
+# A function that returns a dictionary of [position : remoteness] for all position in draw level N where the remoteness of the fringe 
+# of draw level N is 0. all other positions are in there and if the other positions have draw level < N then their value is 0 and if
+# the other positions have draw level > N their value is "UNDECIDED". This function is a strict upgrade from draw_remote()
+# Inputs: su = a dictionary of type [position : value] where the values are of the format that return from up_solve_draw_world_one_tier,
+#              where the value also holds information about the draw level.
+#         n = the draw level you want to solve for.
 def remotenessN(su, n):
     remoteness_dic, ol_remoteness_dic = {}, {}
     avoidList = []
@@ -158,24 +194,37 @@ def remotenessN(su, n):
                 remoteness_dic[p] = aminax(max, [remoteness_dic[q] for q in children if su[q] == 'DRAW ' * n + "WIN"]) + 1
     return remoteness_dic
 
+# Returns a dictionary of type [position : remoteness] that gives a remoteness to all values in the game relative to their draw level.
+# The preferred function for remoteness relative to draw level. EX) all fringe positions of any draw level have remoteness 0
+# Much more useful for conceptual understanding of game trees and the play function than allRemoteGLOBAL().
+# Inputs: su = a dictionary of type [position : value] where the values are of the format that return from up_solve_draw_world_one_tier,
+#              where the value also holds information about the draw level.
 def allRemote(su):
     largest = 0
     for v in su.values():
         largest = max(numDraws(v), largest)
-    remotenessDic = up_remote(su, su.keys())
+    remotenessDic = up_remote(su)
     for i in range(largest):
         justInts = [remotenessDic[m] for m in su.keys() if type(remotenessDic[m]) is int and remotenessDic[m] != float('inf')]
         curLargest = max(justInts)
         trd = remotenessN(su, i + 1)
         for m in [m for m in remotenessDic.keys() if type(remotenessDic[m]) is str or remotenessDic[m] == float('inf') and type(trd[m]) is int and trd[m] != 0]:
-            remotenessDic[m] = trd[m] - 1#+ curLargest + 1
+            remotenessDic[m] = trd[m] - 1
     return remotenessDic
 
+# Returns a dictionary of type [position : remoteness] that gives a remoteness to all values in the game relative to the global count.
+# The preferred function for remoteness relative to the global count. To further explain the global count, the remoteness relative to 
+# draw level is the same as normal, but the fringe of draw level N isn't just 0, it is (the max remoteness of Draw level N-1) + 2.
+# between each draw level there will be a remoteness value that is empty that is used to note the moving of draw level.
+# Much more useful for creating graph visualizations with printViz and bestDrawPlayer mocking than allRemote().
+# Could be improved to run faster by using a combination of numDraws and allRemote.
+# Inputs: su = a dictionary of type [position : value] where the values are of the format that return from up_solve_draw_world_one_tier,
+#              where the value also holds information about the draw level.
 def allRemoteGLOBAL(su):
     largest = 0
     for v in su.values():
         largest = max(numDraws(v), largest)
-    remotenessDic = up_remote(su, su.keys())
+    remotenessDic = up_remote(su)
     for i in range(largest):
         justInts = [remotenessDic[m] for m in su.keys() if type(remotenessDic[m]) is int and remotenessDic[m] != float('inf')]
         curLargest = max(justInts)
@@ -184,16 +233,17 @@ def allRemoteGLOBAL(su):
             remotenessDic[m] = trd[m] + curLargest + 1
     return remotenessDic
 
-def bestDrawPlayer(Position, primitive, generateMoves, doMove, downChild, printBoard, solved={}, remote={}):
+# Makes a move in the game and Returns the new position of the board.
+# Inputs: Position = the current position of the game.
+#         solved = the dictionary of format [position : value]. MUST BE OF FORMAT FROM up_solve_draw_world_one_tier to work
+#         remote = the dicitonary of format [position : remoteness]. MUST BE OF FORMAT FROM allRemoteGLOBAL to work
+def bestDrawPlayer(Position, solved={}, remote={}):
     if solved == {}:
         solved = up_solve_draw_world_one_tier(Position)
     if remote == {}:
         remote = allRemoteGLOBAL(solved)
-    moves = downChild(Position)
+    moves = [do_move(Position, m) for m in generate_moves(Position)]
     SP = solved[Position]
-    #print()
-    #print(remote[Position])
-    #print(SP)
     if SP[len(SP) - 3:] == "WIN":
         lookingFor = "OSE"
     elif SP[len(SP) - 3] == "TIE":
@@ -202,8 +252,6 @@ def bestDrawPlayer(Position, primitive, generateMoves, doMove, downChild, printB
         lookingFor = "WIN"
     SPM = [solved[m] for m in moves]
     nmoves = [m for m in moves if lookingFor == solved[m][len(solved[m]) - 3:]]
-    #print([solved[r] for r in moves])
-    #print([remote[r] for r in moves])
     if lookingFor == "WIN":
         largestRm = 0
         for n in nmoves:
@@ -216,23 +264,43 @@ def bestDrawPlayer(Position, primitive, generateMoves, doMove, downChild, printB
             if remote[n] < largestRm:
                 largestRm = remote[n]
                 m = n
-    #if primitive(m) == "UNDECIDED":
-    #    return bestDrawPlayer(m, primitive, generateMoves, doMove, downChild, printBoard, solved, remote)
     return m
-    #except:
-    #    print("")
-    #except InfiniteRecursionError:
-    #    print
 
-def play(position=None, solved=None, r=None, verbose=True, superVerbose=False, human_turn=True, rG=None):
+# A function that plays the game with one human and one computer player. The computer player is controlled by bestDrawPlayer. If
+# you want to quit halfway just use keyboard force exit. Otherwise exits once game is over.
+# preferred function call is play(). But if you already have access to a solved dictionary of type defined in up_solve_draw_world_one_tier
+# please set solved equal to that dictionary. The same goes for r to format allRemote and rG to format allRemoteGLOBAL.
+# those are not necessary precalculations but would save time on initialization.
+# if you type -v during an input the verbose mode will toggle.
+# if verbose is on the next position will print telling the Value, Remoteness (local), and Draw Level of the position.
+# if you type -sv during an input the super Verbose mode will toggle.
+# if superVerbose is on then the possible moves listed will also contain the Value, Remoteness (local), and draw level of position your
+#     opponent would go to.
+# Inputs: (Optional) position = the position of the current game. default is the initial position of the game
+#         (Optional) solved = a dictionary of type described in up_solve_draw_world_one_tier called on the initial position of the game
+#                             The variable is here to save runtime.
+#         (Optional) r = a dictionary of type described in allRemote called on the initial position solved dictionary. 
+#                        The only acceptable value is allRemote(up_solve_draw_world_one_tier(START_POS)).
+#                        The variable is here to save on runtime.
+#         (Optional) human_turn = a boolean value about whos turn it is. Human or Computer. Default is True, so Human goes first.
+#         (Optional) rG = a dictionary of type described in allRemoteGLOBAL called on the initial position solved dictionary. 
+#                        The only acceptable value is allRemoteGLOBAL(up_solve_draw_world_one_tier(START_POS)).
+#                        The variable is here to save on runtime.
+#         (Optional) printBoard = a function that nearly prints out the current position of nice viewing. Default is the print function
+#         (Optional) verbose = DO NOT OVERRIDE. Variable used for storage purposes
+#         (Optional) superVerbose = DO NOT OVERRIDE. Varible used for storage purposes.
+
+def play(position=None, solved=None, r=None, human_turn=True, rG=None, printBoard=None, verbose=False, superVerbose=False):
     if position == None:
         position = START_POS
     if solved == None:
-        solved = s
+        solved = up_solve_draw_world_one_tier(position)
     if r == None:
         r = allRemote(solved)
     if rG == None:
         rG = allRemoteGLOBAL(solved)
+    if printBoard == None:
+        printBoard = print
     print()
     if r[position] == 0 and numDraws(solved[position]) == 0:
         print("Game over")
@@ -245,7 +313,7 @@ def play(position=None, solved=None, r=None, verbose=True, superVerbose=False, h
         print("You gave computer this board")
     if verbose:
         print("Value:", solved[position], "     Remoteness", r[position], "     Draw level", numDraws(solved[position]))
-    print(position)
+    printBoard(position)
     if human_turn:
         mvs = generate_moves(position)
         for m in range(len(mvs)):
@@ -275,7 +343,7 @@ def play(position=None, solved=None, r=None, verbose=True, superVerbose=False, h
         m = mvs[j]
         np = do_move(position, m)
     else:
-        np = bestDrawPlayer(position, primitive, generate_moves, do_move, down_child, print, solved, rG)
+        np = bestDrawPlayer(position, solved, rG)
     if numDraws(solved[position]) != numDraws(solved[np]) and verbose:
         print()
         if numDraws(solved[position]) > numDraws(solved[np]):
@@ -284,8 +352,66 @@ def play(position=None, solved=None, r=None, verbose=True, superVerbose=False, h
             print("HAHA You have risen from draw level", numDraws(solved[position]), "to draw level", numDraws(solved[np]), "when you had control of the game")
     if solved[position][len(solved[position]) - 3:] == "WIN" and solved[np][len(solved[np]) - 3:] == "WIN":
         print("HAHA you have given up control of the game")
-    return play(position=np, solved=solved, r=r, verbose=verbose, superVerbose=superVerbose, human_turn=not human_turn, rG=rG)
+    return play(position=np, solved=solved, r=r, human_turn=not human_turn, rG=rG, printBoard=printBoard, verbose=verbose, superVerbose=superVerbose)
 
-r = allRemote(s)
-rG = allRemoteGLOBAL(s)
-play()
+# Prints a bunch of code that the GraphViz bundle can understand and create a graph from.
+# Inputs: s = solved dictionary of format [position : value] from up_solve_draw_world_one_tier.
+#         r = remoteness dictionary of format [positon : remoteness] from allRemoteGLOBAL.
+#         m = max(r.values()) just do it
+def printViz(s, r, m):
+    print(" digraph G {")
+    print(" {")
+    print("   node [shape=plaintext];")
+    print()
+    remoteness_line = '0;'
+    for i in range(1, m + 1):
+        remoteness_line = str(i) + " -> " + remoteness_line
+    print(remoteness_line)
+    print(' }')
+    for p in s.keys():
+        color = 'green' if s[p][-3:] == 'WIN' else 'red'
+        shape = 'triangle' if p == START_POS else 'circle'
+        print('node [fontname = "Courier",sytle=filled,color=' + color + ',shape=' + shape + ']')
+        print('  "' + stringify(p) + '" [shape=circle];')
+    counter = 0
+    for position in s.keys():
+        temp = 'temp' + str(counter)
+        done = False
+        print('  { rank=same; ' + str(r[position]) + '; "' + stringify(position) + '";  }')
+        for parent in parents(position, s.keys()):
+            if primitive(parent) == "UNDECIDED":
+                print('  "' + stringify(parent) + '" -> "' + stringify(position) + '" [label=" ", color=black];')
+                done = True
+                counter += 1
+    print('}')
+
+# Returns the draw level of the value given.
+# Inputs: val = the value of the position you want to know the draw level of.
+def numDraws(val):
+    num = 0
+    while True:
+        if (len(val) > 4 and val[:4] == "DRAW"):
+            num += 1
+            val = val[5:]
+        else:
+            return num
+
+# Returns a boolean of whether the game is pure
+# Inputs: s = solved dictionary of format [position : value] from up_solve_draw_world_one_tier
+def isPure(s):
+    for p in s.keys():
+        if len(p) >= 4 and p[-4:] == "LOSE":
+            children = child_solver(p)
+            for c in children:
+                if len(c) >= 4 and c[-4:] == "LOSE":
+                    return False
+    return True
+
+# Returns all the values of the children of a position
+# Inputs: p = a position that you want that you want to know the values of the children of.
+#         su = the solved dictioary of type [position : value]. No need for any specific solver.
+def child_solver(p, su):
+    child_solved_list = []
+    for dc in down_child(p):
+        child_solved_list.append(su[dc])
+    return child_solved_list
