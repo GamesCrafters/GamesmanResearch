2a771a09ef92f8d2ee04c51ab65aa9fb3187e576
Brian Fu <brianfu9@gmail.com>
2019-02-12 14:08:37 -0800

    benchmark

--- cython/benchmark/test.sh
+++ cython/benchmark/test.sh
@@ -5,4 +5,4 @@ python3 -m timeit -s "import test_numpy; import numpy as np; a = np.arange(1e6)"
 # Cython - naive
 python3 -m timeit -s "import pyximport; pyximport.install(language_level=3); import test_cython; import numpy as np; a = np.arange(1e6)" "test_cython.test(a)"
 # Cython calling C
-python3 -m timeit -s "import pyximport; pyximport.install(language_level=3); import test_c; import numpy as np; a = np.arange(1e6)" "test_c.test(a)"
+# python3 -m timeit -s "import pyximport; pyximport.install(language_level=3); import test_c; import numpy as np; a = np.arange(1e6)" "test_c.test(a)"
--- cython/benchmark/test_c.pyx
+++ cython/benchmark/test_c.pyx
@@ -1,7 +1,7 @@
-from numpy cimport ndarray, float64_t
+cimport numpy as np
 
 cdef extern from "std_dev.h":
     double std_dev(double *arr, size_t siz)
 
-def test(ndarray[float64_t, ndim=1] a not None):
+def test(np.ndarray[np.float64_t, ndim=1] a):
     return std_dev(<double*> a.data, a.size)
